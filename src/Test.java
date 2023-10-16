75e795eca993a22109034aeaaa31279194c05218
hientrann <hien.tran@phoenix-software.net>
2023-10-16 19:39:21 +0700

    add them 1

--- src/Test.java
+++ src/Test.java
@@ -2,6 +2,7 @@ public class Test {
     public static void main(String[] args){
         final int num =-19;
         int num2 =20;
+        int num3 =21;
         System.out.println(num);
     }
 }
