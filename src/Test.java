6ea5b2f8953feee548ba82169bfeae07233690c7
hientrann <hien.tran@phoenix-software.net>
2023-10-16 20:06:05 +0700

    add 16/10/23

--- src/Test.java
+++ src/Test.java
@@ -1,8 +1,15 @@
 public class Test {
+    public static final int[] thisarry ={5, 1, 4, 2, 3};
     public static void main(String[] args){
-        final int num =-19;
-        int num2 =20;
-        int num3 =21;
-        System.out.println(num);
+        for (int x = 0; x < 4; ++x){
+            TIM: for(int y = 0; y < 2;++y){
+            for (int z = 0; z < 2; ++z){
+                System.out.println("(x variable, y variable, z variable) = ("+x+","+y+","+z+")");
+                if(x==y && y==z){
+                    break TIM;
+                }
+            }
+            }
+        }
     }
 }
