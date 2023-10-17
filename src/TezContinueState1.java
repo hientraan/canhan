685b3aab6967fe2e8835ef8bf25be802d0fb306e
hientrann <hien.tran@phoenix-software.net>
2023-10-17 19:56:15 +0700

    new TezContinueState1

--- src/TezContinueState1.java
+++ src/TezContinueState1.java
@@ -1,4 +1,4 @@
-public class Test {
+public class TezContinueState1 {
     public static final int[] thisarray ={5, 1, 4, 2, 3};
     public static void main(String[] args){
         for (int x = 0; x < thisarray.length; ++x){
