8476b22b1240a1796984a909f9619d85f8faeb91
hientrann <hien.tran@phoenix-software.net>
2023-10-17 19:30:06 +0700

    add them 1

--- src/TezContinueState.java
+++ src/TezContinueState.java
@@ -1,4 +1,4 @@
-public class Test {
+public class TezContinueState {
     public static final int[] thisarray ={5, 1, 4, 2, 3};
     public static void main(String[] args){
         for (int x = 0; x < 4; ++x){
