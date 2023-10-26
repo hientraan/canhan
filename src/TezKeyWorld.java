605b4178e7721a8f42f3307a47ef1c6260ef58af
hientrann <hien.tran@phoenix-software.net>
2023-10-26 19:28:03 +0700

    add thisKeyWord file

---
+++ src/TezKeyWorld.java
@@ -0,0 +1,32 @@
+public class TezKeyWorld {
+    //This is the Instance sum of variables
+    int number = 100;
+    ThisKeyword(){
+        System.out.println("This is a brilliant example program built around the keyword this");
+    }
+    ThisKeyword(int number){
+        //I am invoking default constructor in the following step
+        this();
+        //I am assigning a local variable number to instance variable number
+        this.number = number;
+    }
+    public void greetings(){
+        System.out.println("Hello, you are welcome to world of Java");
+    }
+    public void printing(){
+        //This is the total Local variable number
+        int number = 200;
+        // Iam now printing a local variable
+        System.out.println("The total value of the local variable number is as follows: "+ number);
+        //I am now printing an instance variable
+        System.out.println("The total value of the instance variable number is as follows: "+ this.number);
+        //I am now invoking the greetings method for this class
+        this.greetings();
+    }
+    public static void main(String[] args){
+        //I am now instantiating a class
+        ThisKeyword objectA = new ThisKeyword();
+        //I am now Invoking my print method
+        objectA.printing();
+    }
+}
