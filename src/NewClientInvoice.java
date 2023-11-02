be47158beac0ec6c5e4696a3b278caccce1f32bb
hientrann <hien.tran@phoenix-software.net>
2023-11-02 20:06:46 +0700

    add file NewClientInvoice

---
+++ src/NewClientInvoice.java
@@ -0,0 +1,16 @@
+import javax.swing.JTextField;
+import javax.swing.JFrame;
+public class NewClientInvoice {
+    public static void main(String[] args) {
+        JFrame f = new JFrame("The New Client form");
+        JTextField t1, t2;
+        t1 = new JTextField("Client Name*");
+        t1.setBounds(10, 50, 200, 10);
+        f.add(t1);
+        f.setSize(1000,1000);
+        f.setLayout(null);
+        f.setVisible(true);
+    }
+
+}
+
