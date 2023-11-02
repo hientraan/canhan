4b7ab044592de3a0cc14b48fbae9162962d746ef
hientrann <hien.tran@phoenix-software.net>
2023-11-02 20:14:56 +0700

    add format file

--- src/NewClientInvoice.java
+++ src/NewClientInvoice.java
@@ -1,5 +1,6 @@
 import javax.swing.JTextField;
 import javax.swing.JFrame;
+
 public class NewClientInvoice {
     public static void main(String[] args) {
         JFrame f = new JFrame("The New Client form");
@@ -7,7 +8,7 @@ public class NewClientInvoice {
         t1 = new JTextField("Client Name*");
         t1.setBounds(10, 50, 200, 10);
         f.add(t1);
-        f.setSize(1000,1000);
+        f.setSize(1000, 1000);
         f.setLayout(null);
         f.setVisible(true);
     }
